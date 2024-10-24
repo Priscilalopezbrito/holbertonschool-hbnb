c0cd263326ffceb521f700fa7f4f05809ca91cf7
priscilalopez <prisscilalb@gmail.com>
2024-10-23 22:03:06 -0400

    Update in facade and users file

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -1,4 +1,6 @@
 from flask_restx import Namespace, Resource, fields
+
+from part2.hbnb.app.models import user
 from part2.hbnb.app.services import facade
 
 api = Namespace('users', description='User operations')
@@ -45,3 +47,30 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+
+
+@api.route('/user-list')
+class Users(Resource):
+    @api.response(200, 'List of users retrieved successfully')
+    def get(self):
+        """List of users"""
+        users = facade.get_all_users()
+        return [{'id': user.id,
+                 'first_name': user.first_name,
+                 'last_name': user.last_name,
+                 'email': user.email} for user in users], 200
+
+
+@api.route('/update/<user_id>')
+class UserResource(Resource):
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User updated successfully')
+    @api.response(404, 'User not found')
+    def put(self, user_id):
+        """Update user information"""
+        user_data = api.payload
+        updated_user = facade.update_user(user_id, user_data)
+        if not updated_user:
+            return {'error': 'User not found'}, 404
+        return {'id': updated_user.id, 'first_name': updated_user.first_name, 'last_name': updated_user.last_name, 'email': updated_user.email}, 200
+
